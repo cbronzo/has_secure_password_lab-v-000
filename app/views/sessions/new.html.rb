ffaea7c6676142c5e0e9413fabe5115e8c322803
Catherine Bronzo <cbronzo@mac.com>
2018-10-05 21:07:37 +0000

    automatically backed up by learn

--- app/views/sessions/new.html.rb
+++ app/views/sessions/new.html.rb
@@ -1,7 +1,6 @@
 <%= form_for :session, url: '/sessions', method: 'POST' do |f| %>
   Name:
   <%= f.text_field :name %>
-  Password:
   <%= f.text_field :password %>
   <%= f.submit 'Log In' %>
 <% end %>
