5957c8345b8c05db6d99dc246e0b9bbb7760bec0
Catherine Bronzo <cbronzo@mac.com>
2018-10-05 21:04:21 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,12 @@ class SessionsController < ApplicationController
   
   def create
     @user = User.find_by(name: params[:user][:name])
-    @user = @user.try(:authenticate, params[:user][:password])
-    return redirect_to '/users/new' unless @user
-    session[:user_id] = @user.id
-    redirect_to '/welcome'
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
   
   def destroy
