761095540ff528d68ec0f2a01f7d5a5c519d5121
Catherine Bronzo <cbronzo@mac.com>
2018-10-05 21:08:15 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      render :new
+      redirect_to login_path
     end
   end
   
