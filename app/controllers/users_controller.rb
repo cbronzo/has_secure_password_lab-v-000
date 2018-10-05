71bd93633a0bd292ca8cc88ad9098edcc260f3a6
Catherine Bronzo <cbronzo@mac.com>
2018-10-05 21:08:37 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to '/welcome'
     else
-      redirect_to new_users_path
+      redirect_to new_user_path
     end
   end
   
